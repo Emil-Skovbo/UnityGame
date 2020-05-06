bc7b0e0dbb517bcaa644eee4331732890f1db55c
Emil <emilskovbo18@gmail.com>
2020-05-06 15:12:04 +0200

    backup

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Rigidbody rb;
+    public NavMeshAgent agent;
+    public Transform target;
+    
+    void Start()
+    {
+        //target = PlayerManager.instance.player.transform; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceTarget();
+        movement();
+    }
+
+    void faceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotaion = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotaion, Time.deltaTime * 5f);
+    }
+
+    void movement()
+    {
+        agent.SetDestination(target.position);
+        if (gameObject.tag == "Enemy2")
+        {
+            agent.baseOffset = Mathf.Sin(Mathf.PI * (target.position.x + Time.deltaTime));
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Hero")
+        {
+
+        }
+    }
+
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+    public int xPos;
+    public int zPos;
+    public int enemy1CountAmount;
+    public int enemy2CountAmount;
+    private int enemyCounter1 = 0;
+    private int enemyCounter2 = 0;
+    
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+
+    IEnumerator EnemyDrop()
+    {
+        while(enemyCounter1 < enemy1CountAmount)
+        {
+            xPos = Random.Range(1, 50);
+            zPos = Random.Range(1, 50);
+            Instantiate(Enemy1, new Vector3(xPos, 5, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCounter1 += 1;
+        }
+        while (enemyCounter2 < enemy2CountAmount)
+        {
+            xPos = Random.Range(1, 50);
+            zPos = Random.Range(1, 50);
+            Instantiate(Enemy2, new Vector3(xPos, 5, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCounter2 += 1;
+        }
+    }
+}
